3a638e3c00ef2d97ee6ab996714d16ce16fe9720
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-01-30 13:16:41 -0600

    Add a html title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
+import Head from "next/head";
+import axios from "axios";
+
 import "../reset.css";
 import AppContext from "@contexts/AppContext";
 import useInitialState from "@hooks/useInitialState";
-import axios from "axios";
 
 function MyApp({ Component, pageProps }) {
   const initialState = useInitialState();
   axios.defaults.baseURL = process.env.API_URL;
   return (
-    <AppContext.Provider value={initialState}>
-      <Component {...pageProps} />;
-    </AppContext.Provider>
+    <>
+      <Head>
+        <title>Historial Medico</title>
+      </Head>
+      <AppContext.Provider value={initialState}>
+        <Component {...pageProps} />;
+      </AppContext.Provider>
+    </>
   );
 }
 
